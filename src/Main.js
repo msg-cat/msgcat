4ce9e697ada74a8c81580d684c57fb9b993c816d
franz vader <franz.vader@deep.web>
2016-04-02 21:39:46 +0200

    add SideBar and ChatWindow 2 main

--- src/Main.js
+++ src/Main.js
@@ -1,14 +1,19 @@
 import React from 'react';
+import SideBar from './SideBar';
+import Chat from './Chat';
 
 export default class Main extends React.Component {
   constructor(props) {
     super(props);
     this.state = {};
   }
-  
+
   render() {
     return (
-      <div id="main"></div>
+      <div id="main">
+        <SideBar />
+        <Chat />
+      </div>
     );
   }
 }
---
+++ src/SideBar.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+export default class SideBar extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <div id="SideBar">
+        <span>some stuff</span>
+      </div>
+    );
+  }
+}
